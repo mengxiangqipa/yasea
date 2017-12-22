086d7aee403c24f0aee5a0cc878b8667f3104c5a
YobertJomi <mengxiangqipa@163.com>
2017-12-22 14:48:37 +0800

    2017.12.22

--- app/src/main/java/net/ossrs/yasea/demo/MainActivity.java
+++ app/src/main/java/net/ossrs/yasea/demo/MainActivity.java
@@ -29,73 +29,77 @@ import java.net.SocketException;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements RtmpHandler.RtmpListener,
-                        SrsRecordHandler.SrsRecordListener, SrsEncodeHandler.SrsEncodeListener {
-
+        SrsRecordHandler.SrsRecordListener, SrsEncodeHandler.SrsEncodeListener
+{
     private static final String TAG = "Yasea";
-
     private Button btnPublish;
     private Button btnSwitchCamera;
     private Button btnRecord;
     private Button btnSwitchEncoder;
-
     private SharedPreferences sp;
     private String rtmpUrl = "rtmp://ossrs.net/" + getRandomAlphaString(3) + '/' + getRandomAlphaDigitString(5);
+//    private String rtmpUrl = "rtmp://winlinvip.github.io:1935/live/demo";
     private String recPath = Environment.getExternalStorageDirectory().getPath() + "/test.mp4";
-
     private SrsPublisher mPublisher;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
-
         // response screen rotation event
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
-
         // restore data.
         sp = getSharedPreferences("Yasea", MODE_PRIVATE);
         rtmpUrl = sp.getString("rtmpUrl", rtmpUrl);
-
         // initialize url.
         final EditText efu = (EditText) findViewById(R.id.url);
         efu.setText(rtmpUrl);
-
         btnPublish = (Button) findViewById(R.id.publish);
         btnSwitchCamera = (Button) findViewById(R.id.swCam);
         btnRecord = (Button) findViewById(R.id.record);
         btnSwitchEncoder = (Button) findViewById(R.id.swEnc);
-
         mPublisher = new SrsPublisher((SrsCameraView) findViewById(R.id.glsurfaceview_camera));
         mPublisher.setEncodeHandler(new SrsEncodeHandler(this));
         mPublisher.setRtmpHandler(new RtmpHandler(this));
         mPublisher.setRecordHandler(new SrsRecordHandler(this));
-        mPublisher.setPreviewResolution(640, 360);
-        mPublisher.setOutputResolution(360, 640);
+
+//        mPublisher.setPreviewResolution(640, 360);
+//        mPublisher.setOutputResolution(360, 640);
+
+        mPublisher.setPreviewResolution(960, 540);
+        mPublisher.setOutputResolution(540, 960);
+
+//        mPublisher.setPreviewResolution(320, 180);
+//        mPublisher.setOutputResolution(180, 320);
+
         mPublisher.setVideoHDMode();
         mPublisher.startCamera();
-
-        btnPublish.setOnClickListener(new View.OnClickListener() {
+        btnPublish.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                if (btnPublish.getText().toString().contentEquals("publish")) {
+            public void onClick(View v)
+            {
+                if (btnPublish.getText().toString().contentEquals("publish"))
+                {
                     rtmpUrl = efu.getText().toString();
                     SharedPreferences.Editor editor = sp.edit();
                     editor.putString("rtmpUrl", rtmpUrl);
                     editor.apply();
-
                     mPublisher.startPublish(rtmpUrl);
                     mPublisher.startCamera();
-
-                    if (btnSwitchEncoder.getText().toString().contentEquals("soft encoder")) {
+                    if (btnSwitchEncoder.getText().toString().contentEquals("soft encoder"))
+                    {
                         Toast.makeText(getApplicationContext(), "Use hard encoder", Toast.LENGTH_SHORT).show();
-                    } else {
+                    } else
+                    {
                         Toast.makeText(getApplicationContext(), "Use soft encoder", Toast.LENGTH_SHORT).show();
                     }
                     btnPublish.setText("stop");
                     btnSwitchEncoder.setEnabled(false);
-                } else if (btnPublish.getText().toString().contentEquals("stop")) {
+                } else if (btnPublish.getText().toString().contentEquals("stop"))
+                {
                     mPublisher.stopPublish();
                     mPublisher.stopRecord();
                     btnPublish.setText("publish");
@@ -104,38 +108,47 @@ public class MainActivity extends AppCompatActivity implements RtmpHandler.RtmpL
                 }
             }
         });
-
-        btnSwitchCamera.setOnClickListener(new View.OnClickListener() {
+        btnSwitchCamera.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 mPublisher.switchCameraFace((mPublisher.getCamraId() + 1) % Camera.getNumberOfCameras());
             }
         });
-
-        btnRecord.setOnClickListener(new View.OnClickListener() {
+        btnRecord.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                if (btnRecord.getText().toString().contentEquals("record")) {
-                    if (mPublisher.startRecord(recPath)) {
+            public void onClick(View v)
+            {
+                if (btnRecord.getText().toString().contentEquals("record"))
+                {
+                    if (mPublisher.startRecord(recPath))
+                    {
                         btnRecord.setText("pause");
                     }
-                } else if (btnRecord.getText().toString().contentEquals("pause")) {
+                } else if (btnRecord.getText().toString().contentEquals("pause"))
+                {
                     mPublisher.pauseRecord();
                     btnRecord.setText("resume");
-                } else if (btnRecord.getText().toString().contentEquals("resume")) {
+                } else if (btnRecord.getText().toString().contentEquals("resume"))
+                {
                     mPublisher.resumeRecord();
                     btnRecord.setText("pause");
                 }
             }
         });
-
-        btnSwitchEncoder.setOnClickListener(new View.OnClickListener() {
+        btnSwitchEncoder.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                if (btnSwitchEncoder.getText().toString().contentEquals("soft encoder")) {
+            public void onClick(View v)
+            {
+                if (btnSwitchEncoder.getText().toString().contentEquals("soft encoder"))
+                {
                     mPublisher.switchToSoftEncoder();
                     btnSwitchEncoder.setText("hard encoder");
-                } else if (btnSwitchEncoder.getText().toString().contentEquals("hard encoder")) {
+                } else if (btnSwitchEncoder.getText().toString().contentEquals("hard encoder"))
+                {
                     mPublisher.switchToHardEncoder();
                     btnSwitchEncoder.setText("soft encoder");
                 }
@@ -144,24 +157,28 @@ public class MainActivity extends AppCompatActivity implements RtmpHandler.RtmpL
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
-        } else {
-            switch (id) {
+        } else
+        {
+            switch (id)
+            {
                 case R.id.cool_filter:
                     mPublisher.switchCameraFilter(MagicFilterType.COOL);
                     break;
@@ -211,12 +228,12 @@ public class MainActivity extends AppCompatActivity implements RtmpHandler.RtmpL
             }
         }
         setTitle(item.getTitle());
-
         return super.onOptionsItemSelected(item);
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
         super.onResume();
         final Button btn = (Button) findViewById(R.id.publish);
         btn.setEnabled(true);
@@ -224,187 +241,221 @@ public class MainActivity extends AppCompatActivity implements RtmpHandler.RtmpL
     }
 
     @Override
-    protected void onPause() {
+    protected void onPause()
+    {
         super.onPause();
         mPublisher.pauseRecord();
     }
 
     @Override
-    protected void onDestroy() {
+    protected void onDestroy()
+    {
         super.onDestroy();
         mPublisher.stopPublish();
         mPublisher.stopRecord();
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
+    public void onConfigurationChanged(Configuration newConfig)
+    {
         super.onConfigurationChanged(newConfig);
         mPublisher.stopEncode();
         mPublisher.stopRecord();
         btnRecord.setText("record");
         mPublisher.setScreenOrientation(newConfig.orientation);
-        if (btnPublish.getText().toString().contentEquals("stop")) {
+        if (btnPublish.getText().toString().contentEquals("stop"))
+        {
             mPublisher.startEncode();
         }
         mPublisher.startCamera();
     }
 
-    private static String getRandomAlphaString(int length) {
+    private static String getRandomAlphaString(int length)
+    {
         String base = "abcdefghijklmnopqrstuvwxyz";
         Random random = new Random();
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++)
+        {
             int number = random.nextInt(base.length());
             sb.append(base.charAt(number));
         }
         return sb.toString();
     }
 
-    private static String getRandomAlphaDigitString(int length) {
+    private static String getRandomAlphaDigitString(int length)
+    {
         String base = "abcdefghijklmnopqrstuvwxyz0123456789";
         Random random = new Random();
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++)
+        {
             int number = random.nextInt(base.length());
             sb.append(base.charAt(number));
         }
         return sb.toString();
     }
 
-    private void handleException(Exception e) {
-        try {
+    private void handleException(Exception e)
+    {
+        try
+        {
             Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
             mPublisher.stopPublish();
             mPublisher.stopRecord();
             btnPublish.setText("publish");
             btnRecord.setText("record");
             btnSwitchEncoder.setEnabled(true);
-        } catch (Exception e1) {
+        } catch (Exception e1)
+        {
             //
         }
     }
-
     // Implementation of SrsRtmpListener.
 
     @Override
-    public void onRtmpConnecting(String msg) {
+    public void onRtmpConnecting(String msg)
+    {
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onRtmpConnected(String msg) {
+    public void onRtmpConnected(String msg)
+    {
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onRtmpVideoStreaming() {
+    public void onRtmpVideoStreaming()
+    {
     }
 
     @Override
-    public void onRtmpAudioStreaming() {
+    public void onRtmpAudioStreaming()
+    {
     }
 
     @Override
-    public void onRtmpStopped() {
+    public void onRtmpStopped()
+    {
         Toast.makeText(getApplicationContext(), "Stopped", Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onRtmpDisconnected() {
+    public void onRtmpDisconnected()
+    {
         Toast.makeText(getApplicationContext(), "Disconnected", Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onRtmpVideoFpsChanged(double fps) {
+    public void onRtmpVideoFpsChanged(double fps)
+    {
         Log.i(TAG, String.format("Output Fps: %f", fps));
     }
 
     @Override
-    public void onRtmpVideoBitrateChanged(double bitrate) {
+    public void onRtmpVideoBitrateChanged(double bitrate)
+    {
         int rate = (int) bitrate;
-        if (rate / 1000 > 0) {
+        if (rate / 1000 > 0)
+        {
             Log.i(TAG, String.format("Video bitrate: %f kbps", bitrate / 1000));
-        } else {
+        } else
+        {
             Log.i(TAG, String.format("Video bitrate: %d bps", rate));
         }
     }
 
     @Override
-    public void onRtmpAudioBitrateChanged(double bitrate) {
+    public void onRtmpAudioBitrateChanged(double bitrate)
+    {
         int rate = (int) bitrate;
-        if (rate / 1000 > 0) {
+        if (rate / 1000 > 0)
+        {
             Log.i(TAG, String.format("Audio bitrate: %f kbps", bitrate / 1000));
-        } else {
+        } else
+        {
             Log.i(TAG, String.format("Audio bitrate: %d bps", rate));
         }
     }
 
     @Override
-    public void onRtmpSocketException(SocketException e) {
+    public void onRtmpSocketException(SocketException e)
+    {
         handleException(e);
     }
 
     @Override
-    public void onRtmpIOException(IOException e) {
+    public void onRtmpIOException(IOException e)
+    {
         handleException(e);
     }
 
     @Override
-    public void onRtmpIllegalArgumentException(IllegalArgumentException e) {
+    public void onRtmpIllegalArgumentException(IllegalArgumentException e)
+    {
         handleException(e);
     }
 
     @Override
-    public void onRtmpIllegalStateException(IllegalStateException e) {
+    public void onRtmpIllegalStateException(IllegalStateException e)
+    {
         handleException(e);
     }
-
     // Implementation of SrsRecordHandler.
 
     @Override
-    public void onRecordPause() {
+    public void onRecordPause()
+    {
         Toast.makeText(getApplicationContext(), "Record paused", Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onRecordResume() {
+    public void onRecordResume()
+    {
         Toast.makeText(getApplicationContext(), "Record resumed", Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onRecordStarted(String msg) {
+    public void onRecordStarted(String msg)
+    {
         Toast.makeText(getApplicationContext(), "Recording file: " + msg, Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onRecordFinished(String msg) {
+    public void onRecordFinished(String msg)
+    {
         Toast.makeText(getApplicationContext(), "MP4 file saved: " + msg, Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onRecordIOException(IOException e) {
+    public void onRecordIOException(IOException e)
+    {
         handleException(e);
     }
 
     @Override
-    public void onRecordIllegalArgumentException(IllegalArgumentException e) {
+    public void onRecordIllegalArgumentException(IllegalArgumentException e)
+    {
         handleException(e);
     }
-
     // Implementation of SrsEncodeHandler.
 
     @Override
-    public void onNetworkWeak() {
+    public void onNetworkWeak()
+    {
         Toast.makeText(getApplicationContext(), "Network weak", Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onNetworkResume() {
+    public void onNetworkResume()
+    {
         Toast.makeText(getApplicationContext(), "Network resume", Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onEncodeIllegalArgumentException(IllegalArgumentException e) {
+    public void onEncodeIllegalArgumentException(IllegalArgumentException e)
+    {
         handleException(e);
     }
 }
